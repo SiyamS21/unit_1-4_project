b7a20884f80226861c68a41331a6782be3202478
SiyamS21 <email@email.com>
2023-11-09 12:54:58 -0500

    new stuff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = s.nextLine();
+        System.out.print("Enter your grade (9 - 11) : ");
+        String unconvertedGrade = s.nextLine();
+        int grade = Integer.parseInt(unconvertedGrade);
+        Student student = new Student(name, grade);
+        System.out.println("Name is " + name);
+        System.out.println("Grade is " + grade);
+        for (int day = 1; day < 11; day++) {
+            System.out.println("Today is day " + day + "!");
+            for (int period = 1; period < 10; period++) {
+                System.out.println("Period " + period + " is starting!");
+            }
+        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,25 +1,10 @@
 public class Student {
 
-    private String name;
 
-    private int grade;
+    public Student(String name, int grade) {
 
-    private double gpa;
-    public Student (String name, int grade, double gpa) {
-        this.name = name;
-        this.grade = grade;
-        this.gpa = gpa;
+
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public int getGrade() {
-        return grade;
-    }
-
-    public double getGpa() {
-        return gpa;
-    }
 }
